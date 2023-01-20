0cdfff50fb87534ecc61360122f442192443f596
Ground <27danny@naver.com>
2023-01-20 19:22:11 +0900

    Week 1

---
+++ Little Lemon Demo.swiftpm/MyFirstView.swift
@@ -0,0 +1,20 @@
+//
+//  MyFirstView.swift
+//  Little Lemon Demo
+//
+//  Created by KimJS on 2023/01/20.
+//
+
+import SwiftUI
+
+struct MyFirstView: View {
+    var body: some View {
+        Text("Little Lemon")
+    }
+}
+
+struct MyFirstView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyFirstView()
+    }
+}
