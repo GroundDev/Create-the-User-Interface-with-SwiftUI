fe3c5914b853806ff531a9755373501f6df8f185
Ground <27danny@naver.com>
2023-01-20 17:10:44 +0900

    Create a project

---
+++ Reservations/Reservations/ReservationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationsApp.swift
+//  Reservations
+//
+//  Created by KimJS on 2023/01/20.
+//
+
+import SwiftUI
+
+@main
+struct ReservationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
